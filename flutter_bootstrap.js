c564b250023315c52b838bb6e276392a5163ef86
baleyko <baleyko@gmail.com>
2024-08-03 13:18:54 +0000

    Deploying to gh-pages from @ nuclear-waste-storage/temp-2@6473e29bb3b42772819186d133eae4eb5ef0072d 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3978248730"
+    serviceWorkerVersion: "768360943"
   }
 });
